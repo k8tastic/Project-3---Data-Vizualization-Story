be09a07f874975e8adea85fdf4c430a2ee3cb4c7
andrewjgrimaldi <andrewjgrimaldi@aol.com>
2019-04-28 18:15:44 -0400

    added auction locations to map

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,6 +4,148 @@ var myMap = L.map("map", {
   zoom: 11
 });
 
+var circle = L.circle([38.80462,-77.04535], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+var circle1 = L.circle([38.75131,-77.47908], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+
+var circle2 = L.circle([38.81627,-76.75117], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+var circle3 = L.circle([38.84343,-77.3088], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+var circle4 = L.circle([38.96052,-77.08489], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+
+var circle5 = L.circle([38.95931,-77.08444], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+var circle6 = L.circle([38.751308,-77.47908], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+var circle7 = L.circle([38.20099,-77.58743], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+
+var circle8 = L.circle([39.1158,-77.5631], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+
+var circle9 = L.circle([51.508, -0.11], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+var circle10 = L.circle([38.42276,-77.40869], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+var circle11 = L.circle([38.88987,-77.08319], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+var circle12 = L.circle([38.91042,-76.98622], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+
+var circle13 = L.circle([38.5299,-76.97846], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+var circle14 = L.circle([38.94946,-77.08073], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+
+var circle15 = L.circle([39.08351,-77.15099], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+
+var circle16 = L.circle([38.5299,-76.978462], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+
+var circle17 = L.circle([39.12132,-77.23799], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+var circle18 = L.circle([38.94588,-77.0978], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(myMap);
+
+
 // Adding tile layer to the map
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
