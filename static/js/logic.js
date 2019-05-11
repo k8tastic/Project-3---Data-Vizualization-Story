41fac6af518b67d4fcd41c97196dea63e8db6834
Williams, Dartanion De Angelo <d_williams@acs.org>
2019-05-11 10:48:11 -0400

    Cleaning up logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,154 +1,8 @@
-
-
 // Creating map object
 var myMap = L.map("map", {
   center: [38.88, -77.11],
   zoom: 11
 });
-
-/*
-//console.log("myMap");
-var circle = L.circle([38.80462,-77.04535], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-
-var circle1 = L.circle([38.75131,-77.47908], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-
-
-var circle2 = L.circle([38.81627,-76.75117], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-
-var circle3 = L.circle([38.84343,-77.3088], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-
-var circle4 = L.circle([38.96052,-77.08489], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-
-
-var circle5 = L.circle([38.95931,-77.08444], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-
-var circle6 = L.circle([38.751308,-77.47908], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-
-var circle7 = L.circle([38.20099,-77.58743], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-
-
-var circle8 = L.circle([39.1158,-77.5631], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-
-
-var circle9 = L.circle([51.508, -0.11], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-
-var circle10 = L.circle([38.42276,-77.40869], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-
-var circle11 = L.circle([38.88987,-77.08319], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-
-var circle12 = L.circle([38.91042,-76.98622], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-
-
-var circle13 = L.circle([38.5299,-76.97846], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-
-var circle14 = L.circle([38.94946,-77.08073], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-
-
-var circle15 = L.circle([39.08351,-77.15099], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-
-
-var circle16 = L.circle([38.5299,-76.978462], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-
-
-var circle17 = L.circle([39.12132,-77.23799], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-
-var circle18 = L.circle([38.94588,-77.0978], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 50
-}).addTo(myMap);
-*/
 
 // Adding tile layer to the map
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -179,16 +33,12 @@ d3.json(url).then(function(response){
     console.log(response.length)
 
     for (var i = 0; i < response.length; i++) {
-//      console.log("LAT");
-  //    console.log(response[i].lat);
 
       try {
         // code that throws the error
         var latitude = response[i].lat;
         var longitude = response[i].lgn;
 
-        console.log("LAT");
-        console.log(latitude);
         // L.circle([latitude,longitude], {
         //   color: 'red',
         //   fillColor: '#f03',
@@ -208,39 +58,7 @@ d3.json(url).then(function(response){
       }
     }
 
-    // Loop through data
-    // for (var i = 0; i < 10; i++) {
-
-    //   // Set the data location property to a variable
-    //   var latitude = response.lat[i];
-    //   var longitude = response.lgn[i];
-
-    //   console.log(latitude)
-    //   console.log(longitude)
-
-    //   L.circle([latitude,longitude], {
-    // color: 'red',
-    // fillColor: '#f03',
-    // fillOpacity: 0.5,
-    // radius: 50
-    //   }).addTo(myMap);
-
-    //   // // Check for location property
-    //   // if (latitude) {
-
-    //   //   // Add a new marker to the cluster group and bind a pop-up
-    //   //   markers.addLayer(L.marker([longitude, latitude]));
-    //   // //     .bindPopup(response[i].descriptor + "<hr>" + response[i].cross_street_1 + "<br>" + response[i].cross_street_2));
-    //   //  }
-
-    // }
-
     // Add our marker cluster layer to the map
     myMap.addLayer(markers);
-
-
-
-
-
 
   });
